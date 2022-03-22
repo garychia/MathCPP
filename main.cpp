ef6f2b1e30ff055d01821571728ca7a0cd7a65e0
cche0109 <cche0109@student.monash.edu>
2022-03-22 21:59:05 +0800

    Modify main program to test vector ToString function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-#include "vector.hpp"
+#include "vector3d.hpp"
 
 int main(void) {
     Math::Vector3D<int> v1(32, 32, 43);
     Math::Vector3D<double> v2(23, 23.32, 23.23);
-    auto v3 = v1.Add(v2);
-    std::cout << v3.Z << std::endl;
+    Math::Vector3D<float> v3(3.14f, 234.234f, 324.f);
+
+    std::cout << v1 << std::endl;
+    std::cout << v2 << std::endl;
+    std::cout << v3 << std::endl;
     return 0;
 }
