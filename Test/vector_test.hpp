55d33c4c02278dae17a1255ec3b6bfd9c55b2bf8
cche0109 <cche0109@student.monash.edu>
2022-03-22 21:58:31 +0800

    Add unit tests for vector division and cross product

--- Test/vector_test.hpp
+++ Test/vector_test.hpp
@@ -82,17 +82,35 @@ using namespace Math;
     TEST_VECTOR_DOUBLE((v)*s, (v).X *s, (v).Y *s, (v).Z *s) \
     TEST_VECTOR_DOUBLE((v).Scale(s), (v).X *s, (v).Y *s, (v).Z *s)
 
+#define TEST_VECTOR_INT_DIVISION(v, s)                        \
+    TEST_VECTOR_INT((v) / s, (v).X / s, (v).Y / s, (v).Z / s) \
+    TEST_VECTOR_INT((v).Divide(s), (v).X / s, (v).Y / s, (v).Z / s)
+
+#define TEST_VECTOR_FLOAT_DIVISION(v, s)                        \
+    TEST_VECTOR_FLOAT((v) / s, (v).X / s, (v).Y / s, (v).Z / s) \
+    TEST_VECTOR_FLOAT((v).Divide(s), (v).X / s, (v).Y / s, (v).Z / s)
+
+#define TEST_VECTOR_DOUBLE_DIVISION(v, s)                        \
+    TEST_VECTOR_DOUBLE((v) / s, (v).X / s, (v).Y / s, (v).Z / s) \
+    TEST_VECTOR_DOUBLE((v).Divide(s), (v).X / s, (v).Y / s, (v).Z / s)
+
 #define TEST_VECTOR_INT_DOT_PRODUCT(v1, v2) \
-    TEST_VECTOR_INT_OPERATOR(v1, v2, *)     \
     TEST_VECTOR_INT_ARITHMETIC_FUNCTION(v1, v2, Dot, *)
 
 #define TEST_VECTOR_FLOAT_DOT_PRODUCT(v1, v2) \
-    TEST_VECTOR_FLOAT_OPERATION(v1, v2, *)    \
     TEST_VECTOR_FLOAT_ARITHMETIC_FUNCTION(v1, v2, Dot, *)
 
 #define TEST_VECTOR_DOUBLE_DOT_PRODUCT(v1, v2) \
-    TEST_VECTOR_DOUBLE_OPERATION(v1, v2, *)    \
     TEST_VECTOR_DOUBLE_ARITHMETIC_FUNCTION(v1, v2, Dot, *)
+
+#define TEST_VECTOR_INT_CROSS_PRODUCT(v1, v2) \
+    TEST_VECTOR_INT((v1).Cross(v2), (v1).Y *(v2).Z - (v1).Z * (v2).Y, (v1).Z * (v2).X - (v1).X * (v2).Z, (v1).X * (v2).Y - (v1).Y * (v2).X)
+
+#define TEST_VECTOR_FLOAT_CROSS_PRODUCT(v1, v2) \
+    TEST_VECTOR_FLOAT((v1).Cross(v2), (v1).Y *(v2).Z - (v1).Z * (v2).Y, (v1).Z * (v2).X - (v1).X * (v2).Z, (v1).X * (v2).Y - (v1).Y * (v2).X)
+
+#define TEST_VECTOR_DOUBLE_CROSS_PRODUCT(v1, v2) \
+    TEST_VECTOR_DOUBLE((v1).Cross(v2), (v1).Y *(v2).Z - (v1).Z * (v2).Y, (v1).Z * (v2).X - (v1).X * (v2).Z, (v1).X * (v2).Y - (v1).Y * (v2).X)
 
 TEST(Vector3D, Vector3DConstructor)
 {
@@ -143,8 +161,8 @@ TEST(Vector3D, VectorAddition)
     v1Float += v2Float;
     TEST_VECTOR_FLOAT_EQUAL(v1Float, v3Float)
 
-    Vector3D<double> v1Double(31.4f, -20.23f, 23.f);
-    Vector3D<double> v2Double(31.f, -23.f, 93.f);
+    Vector3D<double> v1Double(31.423, -20.233, 23.234);
+    Vector3D<double> v2Double(31.54, -23.234, 93.76);
     Vector3D<double> v3Double = v1Double + v2Double;
     TEST_VECTOR_DOUBLE_ADDITION(v1Double, v2Double)
     v1Double += v2Double;
@@ -174,8 +192,8 @@ TEST(Vector3D, VectorSubtraction)
     v1Float -= v2Float;
     TEST_VECTOR_FLOAT_EQUAL(v1Float, v3Float);
 
-    Vector3D<double> v1Double(31.4f, -20.23f, 23.f);
-    Vector3D<double> v2Double(31.f, -23.f, 93.f);
+    Vector3D<double> v1Double(31.4, -20.23, 23.54);
+    Vector3D<double> v2Double(31.123, -23.55, 93.94);
     Vector3D<double> v3Double = v1Double - v2Double;
     TEST_VECTOR_DOUBLE_SUBTRACTION(v1Double, v2Double)
     v1Double -= v2Double;
@@ -189,23 +207,68 @@ TEST(Vector3D, VectorSubtraction)
     TEST_VECTOR_DOUBLE_EQUAL(v1Int - v1Double, vIntDoubleSubtract)
 }
 
-TEST(Vector3D, VectorScaling)
+TEST(Vector3D, VectorDivision)
 {
     Vector3D<int> v1Int(1, -10, 100);
     Vector3D<int> v2Int = v1Int;
-    TEST_VECTOR_INT_SCALING(v1Int, 34)
-    v1Int *= 2;
-    TEST_VECTOR_INT_EQUAL(v1Int, v2Int * 2)
+    TEST_VECTOR_INT_DIVISION(v1Int, 34)
+    v1Int /= 2;
+    TEST_VECTOR_INT_EQUAL(v1Int, v2Int / 2)
 
     Vector3D<float> v1Float(3.4f, -1320.23f, 23.f);
     Vector3D<float> v2Float = v1Float;
-    TEST_VECTOR_FLOAT_SCALING(v1Float, 324.4f)
-    v1Float *= 10;
-    TEST_VECTOR_FLOAT_EQUAL(v1Float, v2Float * 10)
+    TEST_VECTOR_FLOAT_DIVISION(v1Float, 324.4f)
+    v1Float /= 10;
+    TEST_VECTOR_FLOAT_EQUAL(v1Float, v2Float / 10)
 
-    Vector3D<double> v1Double(31.4f, -20.23f, 23.f);
+    Vector3D<double> v1Double(31.4, -20.23, 23.234);
     Vector3D<double> v2Double = v1Double;
-    TEST_VECTOR_DOUBLE_SCALING(v1Double, 332.343)
-    v1Double *= 3;
-    TEST_VECTOR_DOUBLE_EQUAL(v1Double, v2Double * 3)
+    TEST_VECTOR_DOUBLE_DIVISION(v1Double, 332.343)
+    v1Double /= 3.14;
+    TEST_VECTOR_DOUBLE_EQUAL(v1Double, v2Double / 3.14)
+}
+
+TEST(Vector3D, VectorDotProduct)
+{
+    Vector3D<int> v1Int(1, -10, 100);
+    Vector3D<int> v2Int(32, 17, 34);
+    TEST_VECTOR_INT_DOT_PRODUCT(v1Int, v2Int)
+
+    Vector3D<float> v1Float(3.4f, -1230.23f, 23.f);
+    Vector3D<float> v2Float(24.23f, -23.65f, -67.345f);
+    TEST_VECTOR_FLOAT_DOT_PRODUCT(v1Float, v2Float)
+
+    Vector3D<double> v1Double(31.4, -20.23, 23.234);
+    Vector3D<double> v2Double(9.324, 94.2354, -234.32);
+    TEST_VECTOR_DOUBLE_DOT_PRODUCT(v1Double, v2Double)
+}
+
+TEST(Vector3D, VectorCrossProduct)
+{
+    Vector3D<int> v1Int(1, -310, 45);
+    Vector3D<int> v2Int(23, 56, 94);
+    TEST_VECTOR_INT_CROSS_PRODUCT(v1Int, v2Int)
+
+    Vector3D<float> v1Float(3.4f, -1230.23f, 23.f);
+    Vector3D<float> v2Float(24.23f, -23.65f, -67.345f);
+    TEST_VECTOR_FLOAT_CROSS_PRODUCT(v1Float, v2Float)
+
+    Vector3D<double> v1Double(31.4, -20.23, 23.234);
+    Vector3D<double> v2Double(9.324, 94.2354, -234.32);
+    TEST_VECTOR_DOUBLE_CROSS_PRODUCT(v1Double, v2Double)
+}
+
+TEST(Vector3D, VectorToString)
+{
+    Vector3D<int> v1Int(1, -310, 45);
+    Vector3D<int> v2Int(23, 56, 94);
+    EXPECT_TRUE(v1Int.ToString() == "(1, -310, 45)" && v2Int.ToString() == "(23, 56, 94)");
+
+    Vector3D<float> v1Float(3.4f, -1230.23f, 23.f);
+    Vector3D<float> v2Float(24.23f, -23.65f, -67.345f);
+    EXPECT_TRUE(v1Float.ToString() == "(3.4, -1230.23, 23)" && v2Float.ToString() == "(24.23, -23.65, -67.345)");
+    
+    Vector3D<double> v1Double(31.4, -20.23, 23.234);
+    Vector3D<double> v2Double(9.324, 94.2354, -234.32);
+    EXPECT_TRUE(v1Double.ToString() == "(31.4, -20.23, 23.234)" && v2Double.ToString() == "(9.324, 94.2354, -234.32)");
 }
