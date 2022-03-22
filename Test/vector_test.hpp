e0fd36e3b5558be728ec310709fd0a0b2252efac
cche0109 <cche0109@student.monash.edu>
2022-03-22 18:03:58 +0800

    Add test cases for vector addition and subtraction

--- Test/vector_test.hpp
+++ Test/vector_test.hpp
@@ -5,45 +5,126 @@
 using namespace Math;
 
 #define TEST_VECTOR_INT(v, a, b, c) \
-    EXPECT_EQ((v).x, a);        \
-    EXPECT_EQ((v).y, b);        \
-    EXPECT_EQ((v).z, c);
+    EXPECT_EQ((v).X, a);            \
+    EXPECT_EQ((v).Y, b);            \
+    EXPECT_EQ((v).Z, c);
 
 #define TEST_VECTOR_FLOAT(v, a, b, c) \
-    EXPECT_FLOAT_EQ((v).x, a);        \
-    EXPECT_FLOAT_EQ((v).y, b);        \
-    EXPECT_FLOAT_EQ((v).z, c);
+    EXPECT_FLOAT_EQ((v).X, a);        \
+    EXPECT_FLOAT_EQ((v).Y, b);        \
+    EXPECT_FLOAT_EQ((v).Z, c);
 
 #define TEST_VECTOR_DOUBLE(v, a, b, c) \
-    EXPECT_DOUBLE_EQ((v).x, a);        \
-    EXPECT_DOUBLE_EQ((v).y, b);        \
-    EXPECT_DOUBLE_EQ((v).z, c);
+    EXPECT_DOUBLE_EQ((v).X, a);        \
+    EXPECT_DOUBLE_EQ((v).Y, b);        \
+    EXPECT_DOUBLE_EQ((v).Z, c);
+
+#define TEST_VECTOR_INT_EQUAL(v1, v2) \
+    TEST_VECTOR_INT(v1, (v2).X, (v2).Y, (v2).Z)
+
+#define TEST_VECTOR_FLOAT_EQUAL(v1, v2) \
+    TEST_VECTOR_FLOAT(v1, (v2).X, (v2).Y, (v2).Z)
+
+#define TEST_VECTOR_DOUBLE_EQUAL(v1, v2) \
+    TEST_VECTOR_DOUBLE(v1, (v2).X, (v2).Y, (v2).Z)
+
+#define TEST_VECTOR_INT_OPERATOR(v1, v2, op) \
+    TEST_VECTOR_INT((v1)op(v2), (v1).X op(v2).X, (v1).Y op(v2).Y, (v1).Z op(v2).Z)
+
+#define TEST_VECTOR_FLOAT_OPERATION(v1, v2, op) \
+    TEST_VECTOR_FLOAT((v1)op(v2), (v1).X op(v2).X, (v1).Y op(v2).Y, (v1).Z op(v2).Z)
+
+#define TEST_VECTOR_DOUBLE_OPERATION(v1, v2, op) \
+    TEST_VECTOR_DOUBLE((v1)op(v2), (v1).X op(v2).X, (v1).Y op(v2).Y, (v1).Z op(v2).Z)
+
+#define TEST_VECTOR_INT_ARITHMETIC_FUNCTION(v1, v2, f, op) \
+    TEST_VECTOR_INT((v1).f(v2), (v1).X op(v2).X, (v1).Y op(v2).Y, (v1).Z op(v2).Z)
+
+#define TEST_VECTOR_FLOAT_ARITHMETIC_FUNCTION(v1, v2, f, op) \
+    TEST_VECTOR_FLOAT((v1).f(v2), (v1).X op(v2).X, (v1).Y op(v2).Y, (v1).Z op(v2).Z)
+
+#define TEST_VECTOR_DOUBLE_ARITHMETIC_FUNCTION(v1, v2, f, op) \
+    TEST_VECTOR_DOUBLE((v1).f(v2), (v1).X op(v2).X, (v1).Y op(v2).Y, (v1).Z op(v2).Z)
+
+#define TEST_VECTOR_INT_ADDITION(v1, v2) \
+    TEST_VECTOR_INT_OPERATOR(v1, v2, +)  \
+    TEST_VECTOR_INT_ARITHMETIC_FUNCTION(v1, v2, Add, +)
+
+#define TEST_VECTOR_FLOAT_ADDITION(v1, v2) \
+    TEST_VECTOR_FLOAT_OPERATION(v1, v2, +) \
+    TEST_VECTOR_FLOAT_ARITHMETIC_FUNCTION(v1, v2, Add, +)
+
+#define TEST_VECTOR_DOUBLE_ADDITION(v1, v2) \
+    TEST_VECTOR_DOUBLE_OPERATION(v1, v2, +) \
+    TEST_VECTOR_DOUBLE_ARITHMETIC_FUNCTION(v1, v2, Add, +)
+
+#define TEST_VECTOR_INT_SUBTRACTION(v1, v2) \
+    TEST_VECTOR_INT_OPERATOR(v1, v2, -)     \
+    TEST_VECTOR_INT_ARITHMETIC_FUNCTION(v1, v2, Minus, -)
+
+#define TEST_VECTOR_FLOAT_SUBTRACTION(v1, v2) \
+    TEST_VECTOR_FLOAT_OPERATION(v1, v2, -)    \
+    TEST_VECTOR_FLOAT_ARITHMETIC_FUNCTION(v1, v2, Minus, -)
+
+#define TEST_VECTOR_DOUBLE_SUBTRACTION(v1, v2) \
+    TEST_VECTOR_DOUBLE_OPERATION(v1, v2, -)    \
+    TEST_VECTOR_DOUBLE_ARITHMETIC_FUNCTION(v1, v2, Minus, -)
+
+#define TEST_VECTOR_INT_SCALING(v, s)                    \
+    TEST_VECTOR_INT((v)*s, (v).X *s, (v).Y *s, (v).Z *s) \
+    TEST_VECTOR_INT((v).Scale(s), (v).X *s, (v).Y *s, (v).Z *s)
+
+#define TEST_VECTOR_FLOAT_SCALING(v, s)                    \
+    TEST_VECTOR_FLOAT((v)*s, (v).X *s, (v).Y *s, (v).Z *s) \
+    TEST_VECTOR_FLOAT((v).Scale(s), (v).X *s, (v).Y *s, (v).Z *s)
+
+#define TEST_VECTOR_DOUBLE_SCALING(v, s)                    \
+    TEST_VECTOR_DOUBLE((v)*s, (v).X *s, (v).Y *s, (v).Z *s) \
+    TEST_VECTOR_DOUBLE((v).Scale(s), (v).X *s, (v).Y *s, (v).Z *s)
+
+#define TEST_VECTOR_INT_DOT_PRODUCT(v1, v2) \
+    TEST_VECTOR_INT_OPERATOR(v1, v2, *)     \
+    TEST_VECTOR_INT_ARITHMETIC_FUNCTION(v1, v2, Dot, *)
+
+#define TEST_VECTOR_FLOAT_DOT_PRODUCT(v1, v2) \
+    TEST_VECTOR_FLOAT_OPERATION(v1, v2, *)    \
+    TEST_VECTOR_FLOAT_ARITHMETIC_FUNCTION(v1, v2, Dot, *)
+
+#define TEST_VECTOR_DOUBLE_DOT_PRODUCT(v1, v2) \
+    TEST_VECTOR_DOUBLE_OPERATION(v1, v2, *)    \
+    TEST_VECTOR_DOUBLE_ARITHMETIC_FUNCTION(v1, v2, Dot, *)
 
 TEST(Vector3D, Vector3DConstructor)
 {
     Vector3D<int> v1Int(1, -10, 100);
     Vector3D<int> v2Int(1, -20);
     Vector3D<int> v3Int(100);
+    Vector3D<int> zeroVInt;
 
     Vector3D<float> v1Float(1.f, -10.f, 100.f);
     Vector3D<float> v2Float(1.f, -10.f);
     Vector3D<float> v3Float(1.f);
+    Vector3D<float> zeroVDouble;
 
     Vector3D<double> v1Double(3.14, -6.89, 34.34);
     Vector3D<double> v2Double(3.14, -6.89);
     Vector3D<double> v3Double(3.14);
+    Vector3D<double> zeroVFloat;
 
     TEST_VECTOR_INT(v1Int, 1, -10, 100)
     TEST_VECTOR_INT(v2Int, 1, -20, 0)
     TEST_VECTOR_INT(v3Int, 100, 0, 0)
+    TEST_VECTOR_INT(zeroVInt, 0, 0, 0)
 
     TEST_VECTOR_FLOAT(v1Float, 1.f, -10.f, 100.f)
     TEST_VECTOR_FLOAT(v2Float, 1.f, -10.f, 0.f)
     TEST_VECTOR_FLOAT(v3Float, 1.f, 0.f, 0.f)
+    TEST_VECTOR_FLOAT(zeroVFloat, 0.f, 0.f, 0.f)
 
     TEST_VECTOR_DOUBLE(v1Double, 3.14, -6.89, 34.34)
     TEST_VECTOR_DOUBLE(v2Double, 3.14, -6.89, 0.0)
     TEST_VECTOR_DOUBLE(v3Double, 3.14, 0.0, 0.0)
+    TEST_VECTOR_DOUBLE(zeroVDouble, 0.0, 0.0, 0.0)
 }
 
 TEST(Vector3D, VectorAddition)
@@ -51,44 +132,71 @@ TEST(Vector3D, VectorAddition)
     Vector3D<int> v1Int(1, -10, 100);
     Vector3D<int> v2Int(1, -12, 932);
     Vector3D<int> v3Int = v1Int + v2Int;
-    TEST_VECTOR_INT(v3Int,
-                    v1Int.x + v2Int.x,
-                    v1Int.y + v2Int.y,
-                    v1Int.z + v2Int.z)
-    v3Int = v1Int.Add(v2Int);
-    TEST_VECTOR_INT(v3Int,
-                    v1Int.x + v2Int.x,
-                    v1Int.y + v2Int.y,
-                    v1Int.z + v2Int.z)
-    v3Int += v1Int;
-    TEST_VECTOR_INT(v3Int,
-                    v1Int.x * 2 + v2Int.x,
-                    v1Int.y * 2 + v2Int.y,
-                    v1Int.z * 2 + v2Int.z)
+    TEST_VECTOR_INT_ADDITION(v1Int, v2Int)
+    v1Int += v2Int;
+    TEST_VECTOR_INT_EQUAL(v1Int, v3Int)
 
     Vector3D<float> v1Float(3.4f, -1320.23f, 23.f);
     Vector3D<float> v2Float(231.f, -23.f, 932.f);
-    TEST_VECTOR_FLOAT(
-        v1Float + v2Float,
-        v1Float.x + v2Float.x,
-        v1Float.y + v2Float.y,
-        v1Float.z + v2Float.z)
-    TEST_VECTOR_FLOAT(
-        v1Float.Add(v2Float),
-        v1Float.x + v2Float.x,
-        v1Float.y + v2Float.y,
-        v1Float.z + v2Float.z)
+    Vector3D<float> v3Float = v1Float + v2Float;
+    TEST_VECTOR_FLOAT_ADDITION(v1Float, v2Float)
+    v1Float += v2Float;
+    TEST_VECTOR_FLOAT_EQUAL(v1Float, v3Float)
 
     Vector3D<double> v1Double(31.4f, -20.23f, 23.f);
     Vector3D<double> v2Double(31.f, -23.f, 93.f);
-    TEST_VECTOR_FLOAT(
-        v1Double + v2Double,
-        v1Double.x + v2Double.x,
-        v1Double.y + v2Double.y,
-        v1Double.z + v2Double.z)
-    TEST_VECTOR_FLOAT(
-        v1Double.Add(v2Double),
-        v1Double.x + v2Double.x,
-        v1Double.y + v2Double.y,
-        v1Double.z + v2Double.z)
+    Vector3D<double> v3Double = v1Double + v2Double;
+    TEST_VECTOR_DOUBLE_ADDITION(v1Double, v2Double)
+    v1Double += v2Double;
+    TEST_VECTOR_DOUBLE_EQUAL(v1Double, v3Double)
+
+    Vector3D<float> vIntFloatSum = v1Int + v1Float;
+    TEST_VECTOR_FLOAT_EQUAL(v1Int + v1Float, vIntFloatSum)
+    Vector3D<double> vDoubleFloatSum = v1Float + v1Double;
+    TEST_VECTOR_DOUBLE_EQUAL(v1Float + v1Double, vDoubleFloatSum)
+    Vector3D<double> vIntDoubleSum = v1Int + v1Double;
+    TEST_VECTOR_DOUBLE_EQUAL(v1Int + v1Double, vIntDoubleSum)
+}
+
+TEST(Vector3D, VectorSubtraction)
+{
+    Vector3D<int> v1Int(1, -10, 100);
+    Vector3D<int> v2Int(1, -12, 932);
+    Vector3D<int> v3Int = v1Int - v2Int;
+    TEST_VECTOR_INT_SUBTRACTION(v1Int, v2Int)
+    v1Int -= v2Int;
+    TEST_VECTOR_INT_EQUAL(v1Int, v3Int)
+
+    Vector3D<float> v1Float(3.4f, -1320.23f, 23.f);
+    Vector3D<float> v2Float(231.f, -23.f, 932.f);
+    Vector3D<float> v3Float = v1Float - v2Float;
+    TEST_VECTOR_FLOAT_SUBTRACTION(v1Float, v2Float)
+    v1Float -= v2Float;
+    TEST_VECTOR_FLOAT_EQUAL(v1Float, v3Float);
+
+    Vector3D<double> v1Double(31.4f, -20.23f, 23.f);
+    Vector3D<double> v2Double(31.f, -23.f, 93.f);
+    Vector3D<double> v3Double = v1Double - v2Double;
+    TEST_VECTOR_DOUBLE_SUBTRACTION(v1Double, v2Double)
+    v1Double -= v2Double;
+    TEST_VECTOR_DOUBLE_EQUAL(v1Double, v3Double);
+
+    Vector3D<float> vIntFloatSubtract = v1Int - v1Float;
+    TEST_VECTOR_FLOAT_EQUAL(v1Int - v1Float, vIntFloatSubtract)
+    Vector3D<double> vDoubleFloatSubtract = v1Float - v1Double;
+    TEST_VECTOR_DOUBLE_EQUAL(v1Float - v1Double, vDoubleFloatSubtract)
+    Vector3D<double> vIntDoubleSubtract = v1Int - v1Double;
+    TEST_VECTOR_DOUBLE_EQUAL(v1Int - v1Double, vIntDoubleSubtract)
+}
+
+TEST(Vector3D, VectorScaling)
+{
+    Vector3D<int> v1Int(1, -10, 100);
+    TEST_VECTOR_INT_SCALING(v1Int, 34)
+
+    Vector3D<float> v1Float(3.4f, -1320.23f, 23.f);
+    TEST_VECTOR_FLOAT_SCALING(v1Float, 324.4f)
+
+    Vector3D<double> v1Double(31.4f, -20.23f, 23.f);
+    TEST_VECTOR_DOUBLE_SCALING(v1Double, 332.343)
 }
