5b64a50ea05d6c44f9ae318749745bae412b13eb
cche0109 <cche0109@student.monash.edu>
2022-03-23 23:21:11 +0800

    Add unit tests for Vector

--- Test/vector_test.hpp
+++ Test/vector_test.hpp
@@ -10,3 +10,11 @@ TEST(Vector, VectorConstructor)
     EXPECT_EQ(emptyVector.Size(), 0);
     EXPECT_EQ(emptyVector.Dimension(), 0);
 }
+
+TEST(Vector, ZeroVector)
+{
+    const int length = 50000000;
+    auto zeroVector = Vector<int>::ZeroVector(length);
+    for (int i = 0; i < length; i++)
+        EXPECT_EQ(zeroVector[i], 0);
+}
