da1a8f38af093acd6dd50046eaa424bbce6b76f4
cche0109 <cche0109@student.monash.edu>
2022-03-23 11:56:43 +0800

    Update comments

--- vector.hpp
+++ vector.hpp
@@ -8,54 +8,86 @@
 
 namespace Math
 {
+    /*
+    Vector Class is a container that is capable of storing multiple elements
+    such as numbers.
+    */
     template <class T>
     class Vector : public Container<T>
     {
     public:
-        Vector(std::initializer_list<T> l) : nDimension(l.size())
+        /*
+        Constructor with Initializer List as Input.
+        @param l an initializer_list that contains the elements this Vector will store.
+        */
+        Vector(std::initializer_list<T> l) : dimension(l.size())
         {
             data = new T[l.size()];
             for (std::size_t i = 0; i < l.size(); i++)
                 data[i] = *(l.begin() + i);
         }
 
+        /*
+        Copy Constructor
+        @param other a Vector to be copied.
+        */
         Vector(const Vector<T> &other)
         {
-            T *newData = new T[other.nDimension];
-            for (std::size_t i = 0; i < other.nDimension; i++)
+            T *newData = new T[other.dimension];
+            for (std::size_t i = 0; i < other.dimension; i++)
                 newData[i] = other.data[i];
             delete[] data;
             data = newData;
         }
 
+        /*
+        Move Constructor
+        @param other a Vector to be moved.
+        */
         Vector(Vector<T> &&other)
         {
-            nDimension = other.nDimension;
+            dimension = other.dimension;
             data = other.data;
-            other.nDimension = 0;
+            other.dimension = 0;
             other.data = nullptr;
         }
 
+        /* Destructor */
         ~Vector()
         {
             delete[] data;
         }
 
+        /*
+        Operator []
+        @param index the index of the element to be accessed.
+        @return the element
+        */
         T &operator[](const std::size_t &index)
         {
-            if (index > nDimension - 1)
+            if (index > dimension - 1)
                 throw Exceptions::IndexOutOfBound(
                     index,
                     "Vector: Index must be less than the dimention.");
             return data[index];
         }
 
-        std::size_t Dimension() const { return nDimension; }
+        /*
+        Returns the dimention of this Vector.
+        @return the dimention of this Vector.
+        */
+        std::size_t Dimension() const { return dimension; }
 
-        std::size_t Size() const { return nDimension; }
+        /*
+        Returns the number of elements this Vector contains.
+        @return the number of elements this Vector contains.
+        */
+        std::size_t Size() const { return dimension; }
 
     private:
-        std::size_t nDimension;
+        // the dimension of this Vector
+        std::size_t dimension;
+        // an array that contians the elements
         T *data;
     };
 }
