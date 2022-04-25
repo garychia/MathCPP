a31cfe8a1e602e5b4dd1d07dbc6f3b0dcf1d5840
cche0109 <cche0109@student.monash.edu>
2022-04-25 23:27:10 +0800

    Implement the Averaged Perceptron algorithm

--- Algorithms/ml_algs.hpp
+++ Algorithms/ml_algs.hpp
@@ -42,4 +42,40 @@ namespace MLAlgs
                 }
         return Vector<double>::Combine({std::move(th), Vector<double>(1, th0)});
     }
+
+    /*
+    The Averaged Perceptron Algorithm.
+    @param data a Matrix with n rows and d columns where n is the number of data points
+    and d is the dimensions of them.
+    @param labels a Vector with a single row and n columns indicating the labels of the
+    data points.
+    @param T the number of iterations to repeat the algorithm (default: 1000).
+    @return a Vector packed with the averaged parameters (theta and offset at the end).
+    */
+    template <class DataType, class LabelType>
+    Vector<double> AveragedPerceptron(const Matrix<DataType>& data, const Vector<LabelType>& labels, std::size_t T = 1000)
+    {
+        const auto dataShape = data.Shape();
+        const auto n = dataShape[0];
+        const auto d = dataShape[1];
+        Vector<double> th(d, 0.0);
+        Vector<double> ths(d, 0.0);
+        double th0 = 0.0;
+        double th0s = 0.0;
+        for (std::size_t i = 0; i < T; i++)
+            for (std::size_t j = 0; j < n; j++)
+            {
+                if (Sign(th.Dot(data[j]) + th0) != labels[j])
+                {
+                    th += data[j] * labels[j];
+                    th0 += labels[j];
+                }
+            ths += th;
+            th0s += th0;
+            }
+        const auto totalIterations = n * T;
+        ths /= totalIterations;
+        th0s /= totalIterations;
+        return Vector<double>::Combine({std::move(ths), Vector<double>(1, th0s)});
+    }
 } // namespace MLAlgs
