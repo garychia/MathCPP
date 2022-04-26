0688348bffda0195e6df0c9b0b9ef11ea7267042
Chia-Hsiang Cheng <cche0109@student.monash.edu>
2022-04-26 19:09:01 +0800

    Separate declaration from implementation

--- DataStructures/list.hpp
+++ DataStructures/list.hpp
@@ -15,256 +15,141 @@ namespace DataStructure
     class List : public Container<T>
     {
     private:
+        // The number of elements this List contains.
         std::size_t nElements;
 
-        void resize()
-        {
-            this->size = this->size > 2 ? this->size << 1 : INITIAL_SIZE;
-            T *newData = new T[this->size];
-            for (std::size_t i = 0; i < nElements; i++)
-                newData[i] = this->data[i];
-            if (this->data)
-                delete[] this->data;
-            this->data = newData;
-        }
+        // Resize the list.
+        void resize();
 
-        void shrink()
-        {
-            this->size = this->size / 2 > INITIAL_SIZE ? this->size / 2 : INITIAL_SIZE;
-            T *newData = new T[this->size];
-            for (std::size_t i = 0; i < nElements; i++)
-                newData[i] = this->data[i];
-            if (this->data)
-                delete[] this->data;
-            this->data = newData;
-        }
+        // Shrink the list.
+        void shrink();
 
     public:
         /*
         Constructor that Generates an Empty List.
         */
-        List() : Container<T>(), nElements(0)
-        {
-            this->data = new T[INITIAL_SIZE];
-            this->size = INITIAL_SIZE;
-        }
+        List();
 
         /*
         Constructor with Initial Size and a Value.
         @param s the initial size of the List to be generated.
         @param value the value the List will be filled with.
         */
-        List(std::size_t s, const T &value) : Container<T>(s, value), nElements(s) {}
+        List(std::size_t s, const T &value);
 
         /*
         Constructor with Initializer List as Input.
         @param l a std::initializer_list that contains the elements this List will store.
         */
-        List(const std::initializer_list<T> &l) : Container<T>(l)
-        {
-            nElements = this->size;
-        }
+        List(const std::initializer_list<T> &l);
 
         /*
         Constructor with an Arrary as Input.
         @param arr a std::array that contains the elements this List will store.
         */
         template <std::size_t N>
-        List(const std::array<T, N> &arr) : Container<T>(arr)
-        {
-            nElements = this->size;
-        }
+        List(const std::array<T, N> &arr);
 
         /*
         Copy Constructor
         @param other a List to be copied.
         */
-        List(const List<T> &other) : Container<T>(other)
-        {
-            nElements = this->nElements;
-        }
+        List(const List<T> &other);
 
         /*
         Move Constructor
         @param other a Vector to be moved.
         */
-        List(List &&other) : Container<T>(other)
-        {
-            nElements = other.nElements;
-        }
+        List(List &&other);
 
         /*
         Copy Assignment
         @param other a List.
         @return a reference to this List.
         */
-        virtual List<T> &operator=(const List<T> &other)
-        {
-            Container<T>::operator=(other);
-            nElements = this->nElements;
-            return *this;
-        }
+        virtual List<T> &operator=(const List<T> &other);
 
         /*
         Operator []
         @param index the index of the element to be accessed.
         @return the element
         */
-        virtual T &operator[](const std::size_t &index)
-        {
-            if (index > nElements - 1)
-                throw Exceptions::IndexOutOfBound(
-                    index,
-                    "Vector: Index must be less than the dimention.");
-            return this->data[index];
-        }
+        virtual T &operator[](const std::size_t &index);
 
         /*
         Operator []
         @param index the index of the element to be accessed.
         @return the element
         */
-        virtual const T &operator[](const std::size_t &index) const override
-        {
-            if (index > nElements - 1)
-                throw Exceptions::IndexOutOfBound(
-                    index,
-                    "List: Index must be less than the number of elements.");
-            return this->data[index];
-        }
+        virtual const T &operator[](const std::size_t &index) const override;
 
         /*
         Returns the number of elements this List stores.
         @return the number of elements this List stores.
         */
-        virtual std::size_t Size() const override { return nElements; }
+        virtual std::size_t Size() const override;
 
         /*
         Checks if this List is empty or not.
         @return a bool that indicates whether this List is empty.
         */
-        virtual bool IsEmpty() const override { return nElements == 0; }
+        virtual bool IsEmpty() const override;
 
         /*
         Converts this List to a string that displays all the elements
         of this List.
         @return a string that represents this List.
         */
-        virtual std::string ToString() const override
-        {
-            if (nElements == 0)
-                return "[]";
-            std::stringstream ss;
-            ss << "[";
-            for (std::size_t i = 0; i < nElements; i++)
-            {
-                ss << this->data[i];
-                if (i < nElements - 1)
-                    ss << ", ";
-            }
-            ss << "]";
-            return ss.str();
-        }
+        virtual std::string ToString() const override;
 
         /*
         Appends an element to this List.
         @param element the element to be appended.
         */
-        virtual void Append(const T &element)
-        {
-            if (nElements + 1 > this->size)
-                resize();
-            this->data[nElements++] = element;
-        }
+        virtual void Append(const T &element);
 
         /*
         Appends an element to this List.
         @param element the element to be appended (and moved).
         */
-        virtual void Append(T &&element)
-        {
-            if (nElements + 1 > this->size)
-                resize();
-            this->data[nElements++] = std::move(element);
-        }
+        virtual void Append(T &&element);
 
         /*
         Prepends an element to this List.
         @param element the element to be prepended.
         */
-        virtual void Prepend(const T &element)
-        {
-            if (nElements + 1 > this->size)
-                resize();
-            nElements++;
-            for (std::size_t i = nElements - 1; i > 0; i--)
-                this->data[i] = this->data[i - 1];
-            this->data[0] = element;
-        }
+        virtual void Prepend(const T &element);
 
         /*
         Prepends an element to this List.
         @param element the element to be prepended (and moved).
         */
-        virtual void Prepend(T &&element)
-        {
-            if (nElements + 1 > this->size)
-                resize();
-            nElements++;
-            for (std::size_t i = nElements - 1; i > 0; i--)
-                this->data[i] = this->data[i - 1];
-            this->data[0] = std::move(element);
-        }
+        virtual void Prepend(T &&element);
 
         /*
         Pops the last element from this List.
         @return the popped element.
         @throw EmptyList when the list is already empty.
         */
-        virtual T PopEnd()
-        {
-            if (nElements == 0)
-                throw Exceptions::EmptyList("List: Trying to pop an element from an empty list.");
-            T element = this->data[--nElements];
-            if (nElements < this->size / 2)
-                shrink();
-            return element;
-        }
+        virtual T PopEnd();
 
         /*
         Pops the first element from this List.
         @return the popped element.
         @throw EmptyList when the list is already empty.
         */
-        virtual T PopFront()
-        {
-            if (nElements == 0)
-                throw Exceptions::EmptyList("List: Trying to pop an element from an empty list.");
-            T element = this->data[0];
-            nElements--;
-            for (std::size_t i = 0; i < nElements; i++)
-                this->data[i] = this->data[i + 1];
-            if (nElements < this->size / 2)
-                shrink();
-            return element;
-        }
+        virtual T PopFront();
 
         /*
         Clears all the elements this List stores.
         */
-        void Clear()
-        {
-            if (this->data) {
-                delete[] this->data;
-                this->data = nullptr;
-            }
-            nElements = 0;
-            shrink();
-        }
+        void Clear();
 
         template <class OtherType>
         friend class List;
     };
 } // namespace DataStructure
+
+#include "list.tpp"
 
 #endif
---
+++ DataStructures/list.tpp
@@ -0,0 +1,187 @@
+namespace DataStructure
+{
+    template <class T>
+    void List<T>::resize()
+    {
+        this->size = this->size > 2 ? this->size << 1 : INITIAL_SIZE;
+        T *newData = new T[this->size];
+        for (std::size_t i = 0; i < nElements; i++)
+            newData[i] = this->data[i];
+        if (this->data)
+            delete[] this->data;
+        this->data = newData;
+    }
+
+    template <class T>
+    void List<T>::shrink()
+    {
+        this->size = this->size / 2 > INITIAL_SIZE ? this->size / 2 : INITIAL_SIZE;
+        T *newData = new T[this->size];
+        for (std::size_t i = 0; i < nElements; i++)
+            newData[i] = this->data[i];
+        if (this->data)
+            delete[] this->data;
+        this->data = newData;
+    }
+
+    template <class T>
+    List<T>::List() : Container<T>(), nElements(0)
+    {
+        this->data = new T[INITIAL_SIZE];
+        this->size = INITIAL_SIZE;
+    }
+
+    template <class T>
+    List<T>::List(std::size_t s, const T &value) : Container<T>(s, value), nElements(s) {}
+
+    template <class T>
+    List<T>::List(const std::initializer_list<T> &l) : Container<T>(l)
+    {
+        nElements = this->size;
+    }
+
+    template <class T>
+    template <std::size_t N>
+    List<T>::List(const std::array<T, N> &arr) : Container<T>(arr)
+    {
+        nElements = this->size;
+    }
+
+    template <class T>
+    List<T>::List(const List<T> &other) : Container<T>(other)
+    {
+        nElements = this->nElements;
+    }
+
+    template <class T>
+    List<T>::List(List &&other) : Container<T>(other)
+    {
+        nElements = other.nElements;
+    }
+
+    template <class T>
+    List<T> &List<T>::operator=(const List<T> &other)
+    {
+        Container<T>::operator=(other);
+        nElements = this->nElements;
+        return *this;
+    }
+
+    template <class T>
+    T &List<T>::operator[](const std::size_t &index)
+    {
+        if (index > nElements - 1)
+            throw Exceptions::IndexOutOfBound(
+                index,
+                "Vector: Index must be less than the dimention.");
+        return this->data[index];
+    }
+
+    template <class T>
+    const T & List<T>::operator[](const std::size_t &index) const
+    {
+        if (index > nElements - 1)
+            throw Exceptions::IndexOutOfBound(
+                index,
+                "List: Index must be less than the number of elements.");
+        return this->data[index];
+    }
+
+    template <class T>
+    std::size_t List<T>::Size() const { return nElements; }
+
+    template <class T>
+    bool List<T>::IsEmpty() const { return nElements == 0; }
+
+    template <class T>
+    std::string List<T>::ToString() const
+    {
+        if (nElements == 0)
+            return "[]";
+        std::stringstream ss;
+        ss << "[";
+        for (std::size_t i = 0; i < nElements; i++)
+        {
+            ss << this->data[i];
+            if (i < nElements - 1)
+                ss << ", ";
+        }
+        ss << "]";
+        return ss.str();
+    }
+
+    template <class T>
+    void List<T>::Append(const T &element)
+    {
+        if (nElements + 1 > this->size)
+            resize();
+        this->data[nElements++] = element;
+    }
+
+    template <class T>
+    void List<T>::Append(T &&element)
+    {
+        if (nElements + 1 > this->size)
+            resize();
+        this->data[nElements++] = std::move(element);
+    }
+
+    template <class T>
+    void List<T>::Prepend(const T &element)
+    {
+        if (nElements + 1 > this->size)
+            resize();
+        nElements++;
+        for (std::size_t i = nElements - 1; i > 0; i--)
+            this->data[i] = this->data[i - 1];
+        this->data[0] = element;
+    }
+
+    template <class T>
+    void List<T>::Prepend(T &&element)
+    {
+        if (nElements + 1 > this->size)
+            resize();
+        nElements++;
+        for (std::size_t i = nElements - 1; i > 0; i--)
+            this->data[i] = this->data[i - 1];
+        this->data[0] = std::move(element);
+    }
+
+    template <class T>
+    T List<T>::PopEnd()
+    {
+        if (nElements == 0)
+            throw Exceptions::EmptyList("List: Trying to pop an element from an empty list.");
+        T element = this->data[--nElements];
+        if (nElements < this->size / 2)
+            shrink();
+        return element;
+    }
+
+    template <class T>
+    T List<T>::PopFront()
+    {
+        if (nElements == 0)
+            throw Exceptions::EmptyList("List: Trying to pop an element from an empty list.");
+        T element = this->data[0];
+        nElements--;
+        for (std::size_t i = 0; i < nElements; i++)
+            this->data[i] = this->data[i + 1];
+        if (nElements < this->size / 2)
+            shrink();
+        return element;
+    }
+
+    template <class T>
+    void List<T>::Clear()
+    {
+        if (this->data)
+        {
+            delete[] this->data;
+            this->data = nullptr;
+        }
+        nElements = 0;
+        shrink();
+    }
+} // namespace DataStructure
