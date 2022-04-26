85e0fc5c21d017e1ea6c1232c288fbe01e6ea4d0
Chia-Hsiang Cheng <cche0109@student.monash.edu>
2022-04-26 19:14:48 +0800

    Separate declaration from implementation

--- DataStructures/tuple.hpp
+++ DataStructures/tuple.hpp
@@ -16,60 +16,60 @@ namespace DataStructure
         /*
         Constructor that Generates an Empty Tuple.
         */
-        Tuple() : Container<T>() {}
+        Tuple();
 
         /*
         Constructor with Initial Size and an Initial Value.
         @param s the initial size of the Tuple to be generated.
         @param value the value the Tuple will be filled with.
         */
-        Tuple(std::size_t s, const T &value) : Container<T>(s, value) {}
+        Tuple(std::size_t s, const T &value);
 
         /*
         Constructor with Initializer List as Input.
         @param l an initializer_list that contains the elements this Tuple will store.
         */
-        Tuple(const std::initializer_list<T>& l) : Container<T>(l) {}
+        Tuple(const std::initializer_list<T>& l);
 
         /*
         Constructor with arrary as Input.
         @param arr an array that contains the elements this Tuple will store.
         */
         template <std::size_t N>
-        Tuple(const std::array<T, N>& arr) : Container<T>(arr) { }
+        Tuple(const std::array<T, N>& arr);
 
         /*
         Constructor with a std::vector.
         @param values a std::vector that contains the elements this Tuple
         will store.
         */
-        Tuple(const std::vector<T> &values) : Container<T>(values) {}
+        Tuple(const std::vector<T> &values);
 
         /*
         Copy Constructor
         @param other a Tuple to be copied.
         */
-        Tuple(const Tuple<T> &other) : Container<T>(other) {}
+        Tuple(const Tuple<T> &other);
 
         /*
         Copy Constructor
         @param other a Tuple to be copied.
         */
         template <class OtherType>
-        Tuple(const Tuple<OtherType> &other) : Container<T>(other) {}
+        Tuple(const Tuple<OtherType> &other);
 
         /*
         Move Constructor
         @param other a Tuple to be moved.
         */
-        Tuple(Tuple<T> &&other) : Container<T>(other) {}
+        Tuple(Tuple<T> &&other);
 
         /*
         Move Constructor
         @param other a Tuple to be moved.
         */
         template<class OtherType>
-        Tuple(Tuple<OtherType> &&other) : Container<T>(other) {}
+        Tuple(Tuple<OtherType> &&other);
 
         /*
         Access the element of a given index.
@@ -77,26 +77,14 @@ namespace DataStructure
         @return the reference of the element accessed.
         @throw IndexOutOfBound when the index exceeds the largest possible index.
         */
-        virtual const T &operator[](const std::size_t &index) const override
-        {
-            if (index < this->size)
-                return this->data[index];
-            else
-                throw Exceptions::IndexOutOfBound(
-                    index,
-                    "Tuple: Index must be non-negative and less than the number of elements.");
-        }
+        virtual const T &operator[](const std::size_t &index) const override;
 
         /*
         Copy Assignment
         @param other a Tuple to be copied.
         @return a reference to this Tuple.
         */
-        virtual Tuple<T> &operator=(const Tuple<T> &other)
-        {
-            Container<T>::operator=(other);
-            return *this;
-        }
+        virtual Tuple<T> &operator=(const Tuple<T> &other);
 
         /*
         Copy Assignment
@@ -105,11 +93,7 @@ namespace DataStructure
         @return a reference to this Tuple.
         */
         template <class OtherType>
-        Tuple<T> &operator=(const Tuple<OtherType> &other)
-        {
-            Container<T>::operator=(other);
-            return *this;
-        }
+        Tuple<T> &operator=(const Tuple<OtherType> &other);
 
         /*
         Given two tuples, check if they have the same elements.
@@ -117,15 +101,7 @@ namespace DataStructure
         elements.
         */
         template <class OtherType>
-        bool operator==(const Tuple<OtherType> &other) const
-        {
-            if (this->Size() != other.Size())
-                return false;
-            for (std::size_t i = 0; i < this->Size(); i++)
-                if ((*this)[i] != other[i])
-                    return false;
-            return true;
-        }
+        bool operator==(const Tuple<OtherType> &other) const;
 
         /*
         Given two tuples, check if they have different elements.
@@ -133,35 +109,20 @@ namespace DataStructure
         elements.
         */
         template <class OtherType>
-        bool operator!=(const Tuple<OtherType> &other) const
-        {
-            return !operator==(other);
-        }
+        bool operator!=(const Tuple<OtherType> &other) const;
 
         /*
         Converts this container to a string that shows the elements
         of this Container.
         @return a string that represents this Container.
         */
-        virtual std::string ToString() const override
-        {
-            if (this->size == 0)
-                return "(EMPTY)";
-            std::stringstream ss;
-            ss << "(";
-            for (std::size_t i = 0; i < this->size; i++)
-            {
-                ss << this->data[i];
-                if (i < this->size - 1)
-                    ss << ", ";
-            }
-            ss << ")";
-            return ss.str();
-        }
+        virtual std::string ToString() const override;
         
         template <class OtherType>
         friend class Tuple;
     };
 } // namespace Math
+
+#include "tuple.tpp"
 
 #endif
---
+++ DataStructures/tuple.tpp
@@ -0,0 +1,95 @@
+namespace DataStructure
+{
+    template <class T>
+    Tuple<T>::Tuple() : Container<T>() {}
+
+    template <class T>
+    Tuple<T>::Tuple(std::size_t s, const T &value) : Container<T>(s, value) {}
+
+    template <class T>
+    Tuple<T>::Tuple(const std::initializer_list<T> &l) : Container<T>(l) {}
+
+    template <class T>
+    template <std::size_t N>
+    Tuple<T>::Tuple(const std::array<T, N> &arr) : Container<T>(arr) {}
+
+    template <class T>
+    Tuple<T>::Tuple(const std::vector<T> &values) : Container<T>(values) {}
+
+    template <class T>
+    Tuple<T>::Tuple(const Tuple<T> &other) : Container<T>(other) {}
+
+    template <class T>
+    template <class OtherType>
+    Tuple<T>::Tuple(const Tuple<OtherType> &other) : Container<T>(other) {}
+
+    template <class T>
+    Tuple<T>::Tuple(Tuple<T> &&other) : Container<T>(other) {}
+
+    template <class T>
+    template <class OtherType>
+    Tuple<T>::Tuple(Tuple<OtherType> &&other) : Container<T>(other) {}
+
+    template <class T>
+    const T &Tuple<T>::operator[](const std::size_t &index) const
+    {
+        if (index < this->size)
+            return this->data[index];
+        else
+            throw Exceptions::IndexOutOfBound(
+                index,
+                "Tuple: Index must be non-negative and less than the number of elements.");
+    }
+
+    template <class T>
+    Tuple<T> &Tuple<T>::operator=(const Tuple<T> &other)
+    {
+        Container<T>::operator=(other);
+        return *this;
+    }
+
+    template <class T>
+    template <class OtherType>
+    Tuple<T> &Tuple<T>::operator=(const Tuple<OtherType> &other)
+    {
+        Container<T>::operator=(other);
+        return *this;
+    }
+
+    template <class T>
+    template <class OtherType>
+    bool Tuple<T>::operator==(const Tuple<OtherType> &other) const
+    {
+        if (this->Size() != other.Size())
+            return false;
+        for (std::size_t i = 0; i < this->Size(); i++)
+            if ((*this)[i] != other[i])
+                return false;
+        return true;
+    }
+
+    template <class T>
+    template <class OtherType>
+    bool Tuple<T>::operator!=(const Tuple<OtherType> &other) const
+    {
+        return !operator==(other);
+    }
+
+    template <class T>
+    std::string Tuple<T>::ToString() const
+    {
+        if (this->size == 0)
+            return "(EMPTY)";
+        std::stringstream ss;
+        ss << "(";
+        for (std::size_t i = 0; i < this->size; i++)
+        {
+            ss << this->data[i];
+            if (i < this->size - 1)
+                ss << ", ";
+        }
+        ss << ")";
+        return ss.str();
+    }
+
+}
