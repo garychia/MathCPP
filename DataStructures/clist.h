15b335c33866a0a2470077f0011a37ba6101ee05
cche0109 <cche0109@student.monash.edu>
2022-04-30 18:37:07 +0800

    Format file

--- DataStructures/clist.h
+++ DataStructures/clist.h
@@ -4,21 +4,22 @@
 #include "clib_macros.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-DEFINE_STRUCT(List, int);
+    DEFINE_STRUCT(List, int);
 
-/* Create a new empty List. */
-GENERATE_FUNCTION_SIGNATURE(int, GENERATE_STRUCT_NAME(List, int)*, create, List);
-/* Create a new List filled with the same value. */
-GENERATE_FUNCTION_SIGNATURE(int, GENERATE_STRUCT_NAME(List, int)*, create_with_same_values, List, int n, int value);
-/* Clone another List. */
-GENERATE_FUNCTION_SIGNATURE(int, GENERATE_STRUCT_NAME(List, int)*, copy, List, const GENERATE_STRUCT_NAME(List, int)* list);
-/* Destroy a list. */
-GENERATE_FUNCTION_SIGNATURE(int, void, destroy, List, GENERATE_STRUCT_NAME(List, int)* list);
-/* Retrieve an element from a list. */
-GENERATE_FUNCTION_SIGNATURE(int, int, get, List, const GENERATE_STRUCT_NAME(List, int)* list, unsigned long index);
+    /* Create a new empty List. */
+    GENERATE_FUNCTION_SIGNATURE(int, GENERATE_STRUCT_NAME(List, int) *, create, List);
+    /* Create a new List filled with the same value. */
+    GENERATE_FUNCTION_SIGNATURE(int, GENERATE_STRUCT_NAME(List, int) *, create_with_same_values, List, int n, int value);
+    /* Clone another List. */
+    GENERATE_FUNCTION_SIGNATURE(int, GENERATE_STRUCT_NAME(List, int) *, copy, List, const GENERATE_STRUCT_NAME(List, int) * list);
+    /* Destroy a list. */
+    GENERATE_FUNCTION_SIGNATURE(int, void, destroy, List, GENERATE_STRUCT_NAME(List, int) * list);
+    /* Retrieve an element from a list. */
+    GENERATE_FUNCTION_SIGNATURE(int, int, get, List, const GENERATE_STRUCT_NAME(List, int) * list, unsigned long index);
 
 #ifdef __cplusplus
 }
