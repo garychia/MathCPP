178dbc1cf13343bbcc9697d6343ed0896be2446d
cche0109 <cche0109@student.monash.edu>
2022-04-30 17:33:23 +0800

    Demonstrate List c functions

---
+++ c_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "DataStructures/clist.h"
+
+int main(void)
+{
+    List_int* l = create_with_same_values_List_int(10, 12);
+    printf("%d\n", get_List_int(l, 9));
+    destroy_List_int(l);
+    return 0;
+}
