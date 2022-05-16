e98eddbc12da32fd05a1659d432ee24e56213333
cche0109 <cche0109@student.monash.edu>
2022-05-16 23:09:13 +0800

    Demonstrate automatic differentiation

--- application.cpp
+++ application.cpp
@@ -8,25 +8,48 @@ using namespace DataStructure;
 int main(void)
 {
     ComputationGraph<float> graph;
-    VariableNode<float> a(&graph, 5);
-    VariableNode<float> b(&graph, 13);
-    VariableNode<float> c(&graph, 23);
+    VariableNode<float> x(&graph, 10);
+    VariableNode<float> p1(&graph, 3);
+    VariableNode<float> p2(&graph, 2);
+    VariableNode<float> p3(&graph, 1);
+    VariableNode<float> c1(&graph, 3);
+    VariableNode<float> c2(&graph, 5);
+    VariableNode<float> c3(&graph, 12);
+    VariableNode<float> c4(&graph, 37);
     auto expression =
         ComputationGraphNode<float>::CombineNodes(
             ComputationGraphNode<float>::CombineNodes(
-                a,
-                b,
-                GraphOperation::Addition),
-            c,
-            GraphOperation::Division);
+                ComputationGraphNode<float>::CombineNodes(
+                    ComputationGraphNode<float>::CombineNodes(
+                        ComputationGraphNode<float>::CombineNodes(
+                            x,
+                            p1,
+                            GraphOperation::Power),
+                        c1,
+                        GraphOperation::Multiplication),
+                    ComputationGraphNode<float>::CombineNodes(
+                        ComputationGraphNode<float>::CombineNodes(
+                            x,
+                            p2,
+                            GraphOperation::Power),
+                        c2,
+                        GraphOperation::Multiplication),
+                    GraphOperation::Addition),
+                ComputationGraphNode<float>::CombineNodes(
+                    ComputationGraphNode<float>::CombineNodes(
+                        x,
+                        p3,
+                        GraphOperation::Power),
+                    c3,
+                    GraphOperation::Multiplication),
+                GraphOperation::Subtraction
+            ),
+            c4,
+            GraphOperation::Addition);
     graph.AddComputation(expression);
-    std::cout << "(a + b) / c = " << graph.Forward() << std::endl;
-    std::cout << "a = " << a.Forward() << std::endl;
-    std::cout << "b = " << b.Forward() << std::endl;
-    std::cout << "c = " << c.Forward() << std::endl;
+    std::cout << "f(x) = 3x^3 + 5x^2 - 12x + 37 = " << graph.Forward() << std::endl;
+    std::cout << "x = " << x.Forward() << std::endl;
     graph.Backward();
-    std::cout << "da = " << a.Backward() << std::endl;
-    std::cout << "db = " << b.Backward() << std::endl;
-    std::cout << "dc = " << c.Backward() << std::endl;
+    std::cout << "dx = " << x.Backward()[0] << std::endl;
     return 0;
 }
