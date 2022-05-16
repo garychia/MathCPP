3c0af224a3feea5490d8e39748d65182250f11d3
cche0109 <cche0109@student.monash.edu>
2022-05-16 19:10:28 +0800

    Demonstrate ComputationGraph

--- application.cpp
+++ application.cpp
@@ -7,19 +7,20 @@ using namespace DataStructure;
 
 int main(void)
 {
-    
     ComputationGraph<float> graph;
-    //auto a = std::make_shared<VariableNode<float>>(12);
-    //auto b = std::make_shared<VariableNode<float>>(32);
-    //std::shared_ptr<FunctionNode<float>> addition = std::make_shared<AddNode<float>>(a, b);
-    //graph.AddComputation(addition);
-    
-    //std::cout << graph.Forward() << std::endl;
-    //graph.Backward();
-    //std::cout << "a = " << a->Forward() << std::endl;
-    //std::cout << "b = " << b->Forward() << std::endl;
-    //std::cout << "da = " << a->Backward() << std::endl;
-    //std::cout << "db = " << b->Backward() << std::endl;
-    
+    VariableNode<float> x(3);
+    VariableNode<float> x2Expo(2);
+    VariableNode<float> xC(3);
+    VariableNode<float> c(85);
+    PowerNode<float> x2Term(&x, &x2Expo);
+    MultiplyNode<float> xTerm(&x, &xC);
+    AddNode<float> add1(&x2Term, &xTerm);
+    AddNode<float> add2(&add1, &c);
+    graph.AddComputation(&add2);
+    graph.Backward();
+    std::cout << "f(x) = x ^ 2 + 3x + 85" << std::endl;
+    std::cout << "x = " << x.Forward() << std::endl;
+    std::cout << "f(x) = " << graph.Forward() << std::endl;
+    std::cout << "dx = " << x.Backward() << std::endl;
     return 0;
 }
