1c6bc81fa50a5d234a76933acf0a1af4cce4019e
cche0109 <cche0109@student.monash.edu>
2022-05-16 23:12:14 +0800

    Format code file

--- application.cpp
+++ application.cpp
@@ -42,14 +42,13 @@ int main(void)
                         GraphOperation::Power),
                     c3,
                     GraphOperation::Multiplication),
-                GraphOperation::Subtraction
-            ),
+                GraphOperation::Subtraction),
             c4,
             GraphOperation::Addition);
     graph.AddComputation(expression);
     std::cout << "f(x) = 3x^3 + 5x^2 - 12x + 37 = " << graph.Forward() << std::endl;
     std::cout << "x = " << x.Forward() << std::endl;
     graph.Backward();
-    std::cout << "dx = " << x.Backward()[0] << std::endl;
+    std::cout << "df(x)/dx = " << x.Backward()[0] << std::endl;
     return 0;
 }
