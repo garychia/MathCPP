6356b7952a143cc3b040e956948346121c4158cd
cche0109 <cche0109@student.monash.edu>
2022-05-17 18:39:49 +0800

    Demonstrate ComputationGraph

--- application.cpp
+++ application.cpp
@@ -8,44 +8,8 @@ using namespace DataStructure;
 int main(void)
 {
     ComputationGraph<float> graph;
-    VariableNode<float> x(&graph, 10);
-    VariableNode<float> p1(&graph, 3);
-    VariableNode<float> p2(&graph, 2);
-    VariableNode<float> p3(&graph, 1);
-    VariableNode<float> c1(&graph, 3);
-    VariableNode<float> c2(&graph, 5);
-    VariableNode<float> c3(&graph, 12);
-    VariableNode<float> c4(&graph, 37);
-    auto expression =
-        ComputationGraphNode<float>::CombineNodes(
-            ComputationGraphNode<float>::CombineNodes(
-                ComputationGraphNode<float>::CombineNodes(
-                    ComputationGraphNode<float>::CombineNodes(
-                        ComputationGraphNode<float>::CombineNodes(
-                            x,
-                            p1,
-                            GraphOperation::Power),
-                        c1,
-                        GraphOperation::Multiplication),
-                    ComputationGraphNode<float>::CombineNodes(
-                        ComputationGraphNode<float>::CombineNodes(
-                            x,
-                            p2,
-                            GraphOperation::Power),
-                        c2,
-                        GraphOperation::Multiplication),
-                    GraphOperation::Addition),
-                ComputationGraphNode<float>::CombineNodes(
-                    ComputationGraphNode<float>::CombineNodes(
-                        x,
-                        p3,
-                        GraphOperation::Power),
-                    c3,
-                    GraphOperation::Multiplication),
-                GraphOperation::Subtraction),
-            c4,
-            GraphOperation::Addition);
-    graph.AddComputation(expression);
+    auto x = graph.CreateVariableNode(12, "x");
+    3 * (x^3) + 5 * (x^2) - 12 * x + 37;
     std::cout << "f(x) = 3x^3 + 5x^2 - 12x + 37 = " << graph.Forward() << std::endl;
     std::cout << "x = " << x.Forward() << std::endl;
     graph.Backward();
