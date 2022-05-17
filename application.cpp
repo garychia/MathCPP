4043dcc08a2f2c56b110ef2cf06b365adc14b1c5
cche0109 <cche0109@student.monash.edu>
2022-05-17 18:54:45 +0800

    Demonstrate partial derivative using ComputationGraph

--- application.cpp
+++ application.cpp
@@ -9,10 +9,13 @@ int main(void)
 {
     ComputationGraph<float> graph;
     auto x = graph.CreateVariableNode(12, "x");
-    3 * (x^3) + 5 * (x^2) - 12 * x + 37;
-    std::cout << "f(x) = 3x^3 + 5x^2 - 12x + 37 = " << graph.Forward() << std::endl;
+    auto y = graph.CreateVariableNode(12, "y");
+    3 * (x^3) + 5 * (x^2) - 12 * x + 37 * y;
+    std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y = " << graph.Forward() << std::endl;
     std::cout << "x = " << x.Forward() << std::endl;
+    std::cout << "y = " << y.Forward() << std::endl;
     graph.Backward();
-    std::cout << "df(x)/dx = " << x.Backward()[0] << std::endl;
+    std::cout << "df(x, y)/dx = " << x.Backward()[0] << std::endl;
+    std::cout << "df(x, y)/dy = " << y.Backward()[0] << std::endl;
     return 0;
 }
