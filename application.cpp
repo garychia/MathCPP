deecf0990daa6999c86d087b49d83d9258fdbbd6
cche0109 <cche0109@student.monash.edu>
2022-05-19 21:46:57 +0800

    Demonstate refactored ComputationGraph

--- application.cpp
+++ application.cpp
@@ -10,12 +10,13 @@ int main(void)
     ComputationGraph<float> graph;
     auto x = graph.CreateVariableNode(12, "x");
     auto y = graph.CreateVariableNode(12, "y");
-    3 * (x^3) + 5 * (x^2) - 12 * x + 37 * y;
+    auto result = 3 * (x^3) + 5 * (x^2) - 12 * x + 37 * y;
+    
     std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y = " << graph.Forward() << std::endl;
-    std::cout << "x = " << x.Forward() << std::endl;
-    std::cout << "y = " << y.Forward() << std::endl;
+    std::cout << "Graph before Backpropagation:" << std::endl;
+    std::cout << graph << std::endl;
     graph.Backward();
-    std::cout << "df(x, y)/dx = " << x.Backward()[0] << std::endl;
-    std::cout << "df(x, y)/dy = " << y.Backward()[0] << std::endl;
+    std::cout << "Graph after Backpropagation:" << std::endl;
+    std::cout << graph << std::endl;
     return 0;
 }
