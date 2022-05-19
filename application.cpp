38dd121d02b34499d737edd6177c3b901dcdf8be
cche0109 <cche0109@student.monash.edu>
2022-05-19 22:38:24 +0800

    Demonstrate changing values of VariableNodes

--- application.cpp
+++ application.cpp
@@ -10,13 +10,22 @@ int main(void)
     ComputationGraph<float> graph;
     auto x = graph.CreateVariableNode(12, "x");
     auto y = graph.CreateVariableNode(12, "y");
-    auto result = 3 * (x^3) + 5 * (x^2) - 12 * x + 37 * y;
+    3 * (x^3) + 5 * (x^2) - 12 * x + 37 * y;
     
     std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y = " << graph.Forward() << std::endl;
-    std::cout << "Graph before Backpropagation:" << std::endl;
-    std::cout << graph << std::endl;
     graph.Backward();
-    std::cout << "Graph after Backpropagation:" << std::endl;
-    std::cout << graph << std::endl;
+    std::cout << "x = " << x.Forward() << std::endl;
+    std::cout << "y = " << y.Forward() << std::endl;
+    std::cout << "df(x, y)/dx = " << x.Gradient() << std::endl;
+    std::cout << "df(x, y)/dy = " << y.Gradient() << std::endl;
+    std::cout << "...........................................\n";
+    x.SetValue(23);
+    y.SetValue(7);
+    std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y = " << graph.Forward() << std::endl;
+    graph.Backward();
+    std::cout << "x = " << x.Forward() << std::endl;
+    std::cout << "y = " << y.Forward() << std::endl;
+    std::cout << "df(x, y)/dx = " << x.Gradient() << std::endl;
+    std::cout << "df(x, y)/dy = " << y.Gradient() << std::endl;
     return 0;
 }
