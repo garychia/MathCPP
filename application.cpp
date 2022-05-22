580923390dc83a98e80eda0bd9bf641d702676c8
Chia-Hsiang Cheng <cche0109@student.monash.edu>
2022-05-22 10:53:08 +0800

    Demonstrate division on ComputationGraph

--- application.cpp
+++ application.cpp
@@ -10,9 +10,9 @@ int main(void)
     ScalerComputationGraph<float> graph;
     auto x = graph.CreateVariableNode(12, "x");
     auto y = graph.CreateVariableNode(12, "y");
-    3 * (x^3) + 5 * (x^2) - 12 * x + 37 * y;
+    3 * (x^3) + 5 * (x^2) - 12 * x + 37 * y + x / y;
     
-    std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y = " << graph.Forward() << std::endl;
+    std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y + x / y = " << graph.Forward() << std::endl;
     graph.Backward();
     std::cout << "x = " << x.Forward() << std::endl;
     std::cout << "y = " << y.Forward() << std::endl;
@@ -21,7 +21,7 @@ int main(void)
     std::cout << "...........................................\n";
     x.SetValue(23);
     y.SetValue(7);
-    std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y = " << graph.Forward() << std::endl;
+    std::cout << "f(x, y) = 3x^3 + 5x^2 - 12x + 37 * y + x / y = " << graph.Forward() << std::endl;
     graph.Backward();
     std::cout << "x = " << x.Forward() << std::endl;
     std::cout << "y = " << y.Forward() << std::endl;
