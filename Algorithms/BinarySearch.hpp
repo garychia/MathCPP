90c878b23eb4ea53c18c324f12a01eb2734d8759
cche0109 <cche0109@student.monash.edu>
2022-06-02 22:42:12 +0800

    Separate implemenation and declaration

--- Algorithms/BinarySearch.hpp
+++ Algorithms/BinarySearch.hpp
@@ -1,8 +1,6 @@
 #ifndef BINARY_SEARCH_HPP
 #define BINARY_SEARCH_HPP
 
-#include <cstddef>
-
 #include "Tuple.hpp"
 
 namespace Algorithms
@@ -15,22 +13,7 @@ namespace Algorithms
     @param end the last index within the search range. (optional)
     */
     template <class IndexType, class ArrayLike, class TargetType, std::size_t ArraySize>
-    IndexType BinarySearch(const ArrayLike &arr, const TargetType &target, IndexType start = 0, IndexType end = -1)
-    {
-        if (end == -1)
-            end = ArraySize - 1;
-        while (start <= end)
-        {
-            const IndexType middle = start + (end - start) / 2;
-            if (arr[middle] == target)
-                return middle;
-            else if (target < arr[middle])
-                end = middle - 1;
-            else
-                start = middle + 1;
-        }
-        return -1;
-    }
+    IndexType BinarySearch(const ArrayLike &arr, const TargetType &target, IndexType start = 0, IndexType end = -1);
 
     /*
     Finds the starting and ending indices of a target.
@@ -40,42 +23,9 @@ namespace Algorithms
     @param end the last index within the search range (inclusive). (optional)
     */
     template <class IndexType, class ArrayLike, class TargetType, std::size_t ArraySize>
-    DataStructure::Tuple<IndexType> SearchRange(const ArrayLike &arr, const TargetType &target, IndexType start = 0, IndexType end = -1)
-    {
-        IndexType startIndex = -1;
-        IndexType endIndex = -1;
-        end = (end == -1) ? ArraySize : end + 1;
-
-        while (start < end)
-        {
-            const IndexType middle = start + (end - start) / 2;
-            if (arr[middle] == target)
-                end = middle;
-            else if (target < arr[middle])
-                end = middle;
-            else
-                start = middle + 1;
-        }
-        if (start >= ArraySize || arr[start] != target)
-            return DataStructure::Tuple<IndexType>(2, -1);
-        
-        startIndex = start;
-        end = ArraySize;
-        while (start < end - 1)
-        {
-            const IndexType middle = start + (end - start) / 2;
-            if (arr[middle] == target)
-                start = middle;
-            else if (target < arr[middle])
-                end = middle;
-            else
-                start = middle + 1;
-        }
-        endIndex = start;
-        if (endIndex >= ArraySize || arr[endIndex] != target)
-            return DataStructure::Tuple<IndexType>(2, -1);
-        return DataStructure::Tuple<IndexType>({startIndex, endIndex});
-    }
+    DataStructure::Tuple<IndexType> SearchRange(const ArrayLike &arr, const TargetType &target, IndexType start = 0, IndexType end = -1);
 } // namespace Algorithms
+
+#include "BinarySearch.tpp"
 
 #endif
---
+++ Algorithms/BinarySearch.tpp
@@ -0,0 +1,58 @@
+namespace Algorithms
+{
+    template <class IndexType, class ArrayLike, class TargetType, std::size_t ArraySize>
+    IndexType BinarySearch(const ArrayLike &arr, const TargetType &target, IndexType start, IndexType end)
+    {
+        if (end == -1)
+            end = ArraySize - 1;
+        while (start <= end)
+        {
+            const IndexType middle = start + (end - start) / 2;
+            if (arr[middle] == target)
+                return middle;
+            else if (target < arr[middle])
+                end = middle - 1;
+            else
+                start = middle + 1;
+        }
+        return -1;
+    }
+
+    template <class IndexType, class ArrayLike, class TargetType, std::size_t ArraySize>
+    DataStructure::Tuple<IndexType> SearchRange(const ArrayLike &arr, const TargetType &target, IndexType start, IndexType end)
+    {
+        IndexType startIndex = -1;
+        IndexType endIndex = -1;
+        end = (end == -1) ? ArraySize : end + 1;
+
+        while (start < end)
+        {
+            const IndexType middle = start + (end - start) / 2;
+            if (arr[middle] == target)
+                end = middle;
+            else if (target < arr[middle])
+                end = middle;
+            else
+                start = middle + 1;
+        }
+        if (start >= ArraySize || arr[start] != target)
+            return DataStructure::Tuple<IndexType>(2, -1);
+
+        startIndex = start;
+        end = ArraySize;
+        while (start < end - 1)
+        {
+            const IndexType middle = start + (end - start) / 2;
+            if (arr[middle] == target)
+                start = middle;
+            else if (target < arr[middle])
+                end = middle;
+            else
+                start = middle + 1;
+        }
+        endIndex = start;
+        if (endIndex >= ArraySize || arr[endIndex] != target)
+            return DataStructure::Tuple<IndexType>(2, -1);
+        return DataStructure::Tuple<IndexType>({startIndex, endIndex});
+    }
+}
