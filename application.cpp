6df4ecb884e5b993bb9822ca05e4a0b8006ae9c0
cche0109 <cche0109@student.monash.edu>
2022-06-10 17:15:35 +0800

    Demonstrate NeuralNetwork.

--- application.cpp
+++ application.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 
 #include "MachineLearning/NeuralNetwork.hpp"
-#include "MachineLearning/LinearLayer.hpp"
+
+#include <stdlib.h>
 
 using namespace DataStructures;
+using namespace MachineLearning;
 
 int main(void)
 {
-    Matrix<double> input({1, 2, 3});
-    MachineLearning::NeuralNetwork network;
-    auto linearLayer = new MachineLearning::LinearLayer(3, 1);
-    network.AddLayer(linearLayer);
-    std::cout << "Input =\n"
-              << input << std::endl;
+    Matrix<double> input({{2, 3, 9, 12}, {5, 2, 6, 5}});
+    Matrix<double> labels({{0, 1, 0, 1}, {1, 0, 1, 0}});
+    List<NeuralNetwork::LayerType> layerTypes({NeuralNetwork::LayerType::Linear});
+    List<Tuple<unsigned int>> shapes({{2, 3}});
+    NeuralNetwork network(layerTypes, shapes);
+    
+    std::cout << "Input =\n" << input << std::endl;
     try
     {
         std::cout << "Prediction =\n"
-              << network.Predict(input) << std::endl;
+                  << network.Predict(input) << std::endl;
     }
     catch (const Exceptions::MatrixShapeMismatch &e)
     {
         std::cout << e.what() << std::endl;
     }
-    delete linearLayer;
     return 0;
 }
