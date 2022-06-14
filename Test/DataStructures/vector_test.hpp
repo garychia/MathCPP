ba60479c3eacee8011815df6ebdc7eba41e3175c
cche0109 <cche0109@student.monash.edu>
2022-06-14 15:14:50 +0800

    Add tests for Vector.

--- Test/DataStructures/vector_test.hpp
+++ Test/DataStructures/vector_test.hpp
@@ -77,6 +77,145 @@ TEST(Vector, VectorArrayConstructor)
         EXPECT_DOUBLE_EQ(initVectorDouble[i++], content);
 }
 
+TEST(Vector, VectorStdVectorConstructor)
+{
+    const std::vector<int> INT_VECTOR_CONTENT({43, -13, 90, -39, 0, 23, -75});
+    const std::vector<float> FLOAT_VECTOR_CONTENT({-2.124f, 23.2f, -82.32f, 84.3f, 1.04f, 0.f, 32.3f, -9.f, 23.f});
+    const std::vector<double> DOUBLE_VECTOR_CONTENT({3.14, -1.24, -0.5576, -94.3, 0.0, 23.0, -7.5, 0.85, 1.23, 2.3423});
+    Vector<int> initVectorInt(INT_VECTOR_CONTENT);
+    Vector<float> initVectorFloat(FLOAT_VECTOR_CONTENT);
+    Vector<double> initVectorDouble(DOUBLE_VECTOR_CONTENT);
+    EXPECT_EQ(initVectorInt.Size(), INT_VECTOR_CONTENT.size());
+    EXPECT_EQ(initVectorFloat.Size(), FLOAT_VECTOR_CONTENT.size());
+    EXPECT_EQ(initVectorDouble.Size(), DOUBLE_VECTOR_CONTENT.size());
+    int i = 0;
+    for (const auto &content : INT_VECTOR_CONTENT)
+        EXPECT_EQ(initVectorInt[i++], content);
+    i = 0;
+    for (const auto &content : FLOAT_VECTOR_CONTENT)
+        EXPECT_FLOAT_EQ(initVectorFloat[i++], content);
+    i = 0;
+    for (const auto &content : DOUBLE_VECTOR_CONTENT)
+        EXPECT_DOUBLE_EQ(initVectorDouble[i++], content);
+}
+
+TEST(Vector, VectorCopyConstructor)
+{
+    const std::vector<int> INT_VECTOR_CONTENT({43, -13, 90, -39, 0, 23, -75});
+    const std::vector<float> FLOAT_VECTOR_CONTENT({-2.124f, 23.2f, -82.32f, 84.3f, 1.04f, 0.f, 32.3f, -9.f, 23.f});
+    const std::vector<double> DOUBLE_VECTOR_CONTENT({3.14, -1.24, -0.5576, -94.3, 0.0, 23.0, -7.5, 0.85, 1.23, 2.3423});
+    Vector<int> initVectorInt(INT_VECTOR_CONTENT);
+    Vector<float> initVectorFloat(FLOAT_VECTOR_CONTENT);
+    Vector<double> initVectorDouble(DOUBLE_VECTOR_CONTENT);
+    Vector<int> initVectorIntCopy(initVectorInt);
+    Vector<float> initVectorFloatCopy(initVectorFloat);
+    Vector<double> initVectorDoubleCopy(initVectorDouble);
+    EXPECT_EQ(initVectorInt.Size(), initVectorIntCopy.Size());
+    EXPECT_EQ(initVectorFloat.Size(), initVectorFloatCopy.Size());
+    EXPECT_EQ(initVectorDouble.Size(), initVectorDoubleCopy.Size());
+    for (int i = 0; i < initVectorInt.Size(); i++)
+        EXPECT_EQ(initVectorInt[i], initVectorIntCopy[i]);
+    for (int i = 0; i < initVectorFloat.Size(); i++)
+        EXPECT_FLOAT_EQ(initVectorFloat[i], initVectorFloatCopy[i]);
+    for (int i = 0; i < initVectorDouble.Size(); i++)
+        EXPECT_DOUBLE_EQ(initVectorDouble[i], initVectorDoubleCopy[i]);
+    // Copy Constructor with different types.
+    // Int to Float.
+    Vector<float> initVectorIntToFloatCopy(initVectorInt);
+    EXPECT_EQ(initVectorIntToFloatCopy.Size(), initVectorInt.Size());
+    for (int i = 0; i < initVectorIntToFloatCopy.Size(); i++)
+        EXPECT_FLOAT_EQ(initVectorIntToFloatCopy[i], initVectorInt[i]);
+    // Float to Double.
+    Vector<float> initVectorFloatToDoubleCopy(initVectorFloat);
+    EXPECT_EQ(initVectorFloatToDoubleCopy.Size(), initVectorFloat.Size());
+    for (int i = 0; i < initVectorFloatToDoubleCopy.Size(); i++)
+        EXPECT_DOUBLE_EQ(initVectorFloatToDoubleCopy[i], initVectorFloat[i]);
+}
+
+TEST(Vector, VectorMoveConstructor)
+{
+    const std::vector<int> INT_VECTOR_CONTENT({43, -13, 90, -39, 0, 23, -75, 23, 35, -93, 75, 46});
+    const std::vector<float> FLOAT_VECTOR_CONTENT({-2.124f, 23.2f, -82.32f, 84.3f, 1.04f, 0.f, 32.3f, -9.f, 23.f});
+    const std::vector<double> DOUBLE_VECTOR_CONTENT({3.14, -1.24, -0.5576, -94.3, 0.0, -3.1343, 23.0, -7.5, 0.85, 1.23, 2.3423});
+    Vector<int> initVectorInt(INT_VECTOR_CONTENT);
+    Vector<float> initVectorFloat(FLOAT_VECTOR_CONTENT);
+    Vector<double> initVectorDouble(DOUBLE_VECTOR_CONTENT);
+    Vector<int> initVectorIntCopy = std::move(initVectorInt);
+    Vector<float> initVectorFloatCopy = std::move(initVectorFloat);
+    Vector<double> initVectorDoubleCopy = std::move(initVectorDouble);
+    EXPECT_EQ(initVectorInt.Size(), ZERO);
+    EXPECT_EQ(initVectorFloat.Size(), ZERO);
+    EXPECT_EQ(initVectorDouble.Size(), ZERO);
+    EXPECT_EQ(initVectorIntCopy.Size(), INT_VECTOR_CONTENT.size());
+    EXPECT_EQ(initVectorFloatCopy.Size(), FLOAT_VECTOR_CONTENT.size());
+    EXPECT_EQ(initVectorDoubleCopy.Size(), DOUBLE_VECTOR_CONTENT.size());
+    for (int i = 0; i < initVectorInt.Size(); i++)
+        EXPECT_EQ(initVectorInt[i], initVectorIntCopy[i]);
+    for (int i = 0; i < initVectorFloat.Size(); i++)
+        EXPECT_FLOAT_EQ(initVectorFloat[i], initVectorFloatCopy[i]);
+    for (int i = 0; i < initVectorDouble.Size(); i++)
+        EXPECT_DOUBLE_EQ(initVectorDouble[i], initVectorDoubleCopy[i]);
+}
+
+TEST(Vector, VectorCopyAssignmentConstructor)
+{
+    const std::vector<int> INT_VECTOR_CONTENT({43, -13, 90, -39, 0, 23, -75});
+    const std::vector<float> FLOAT_VECTOR_CONTENT({-2.124f, 23.2f, -82.32f, 84.3f, 1.04f, 0.f, 32.3f, -9.f, 23.f});
+    const std::vector<double> DOUBLE_VECTOR_CONTENT({3.14, -1.24, -0.5576, -94.3, 0.0, 23.0, -7.5, 0.85, 1.23, 2.3423});
+    Vector<int> initVectorInt(INT_VECTOR_CONTENT);
+    Vector<float> initVectorFloat(FLOAT_VECTOR_CONTENT);
+    Vector<double> initVectorDouble(DOUBLE_VECTOR_CONTENT);
+    Vector<int> initVectorIntCopy = initVectorInt;
+    Vector<float> initVectorFloatCopy = initVectorFloat;
+    Vector<double> initVectorDoubleCopy = initVectorDouble;
+    EXPECT_EQ(initVectorInt.Size(), initVectorIntCopy.Size());
+    EXPECT_EQ(initVectorFloat.Size(), initVectorFloatCopy.Size());
+    EXPECT_EQ(initVectorDouble.Size(), initVectorDoubleCopy.Size());
+    for (int i = 0; i < initVectorInt.Size(); i++)
+        EXPECT_EQ(initVectorInt[i], initVectorIntCopy[i]);
+    for (int i = 0; i < initVectorFloat.Size(); i++)
+        EXPECT_FLOAT_EQ(initVectorFloat[i], initVectorFloatCopy[i]);
+    for (int i = 0; i < initVectorDouble.Size(); i++)
+        EXPECT_DOUBLE_EQ(initVectorDouble[i], initVectorDoubleCopy[i]);
+    // Copy Assignment with different types.
+    // Int to Float.
+    initVectorFloatCopy = initVectorInt;
+    EXPECT_EQ(initVectorFloatCopy.Size(), initVectorInt.Size());
+    for (int i = 0; i < initVectorFloatCopy.Size(); i++)
+        EXPECT_FLOAT_EQ(initVectorFloatCopy[i], initVectorInt[i]);
+    // Float to Double.
+    initVectorDoubleCopy = initVectorFloat;
+    EXPECT_EQ(initVectorDoubleCopy.Size(), initVectorFloat.Size());
+    for (int i = 0; i < initVectorDoubleCopy.Size(); i++)
+        EXPECT_DOUBLE_EQ(initVectorDoubleCopy[i], initVectorFloat[i]);
+}
+
+TEST(Vector, VectorElementAccess)
+{
+    std::vector<int> elements;
+    for (int i = -100; i < 101; i++)
+        elements.push_back(i);
+    Vector<int> t(elements);
+    for (int i = 0; i < elements.size(); i++)
+        EXPECT_EQ(elements[i], t[i]);
+    EXPECT_THROW(
+        {
+            try
+            {
+                t[elements.size()];
+            }
+            catch (const Exceptions::IndexOutOfBound &e)
+            {
+                std::stringstream ss;
+                ss << "Index Out of Bound: " << elements.size() << "\n";
+                ss << "Vector: Index must be less than the dimension.";
+                EXPECT_TRUE(ss.str() == e.what());
+                throw e;
+            }
+        },
+        Exceptions::IndexOutOfBound);
+}
+
 TEST(Vector, ZeroVector)
 {
     const int VECTOR_LENGHTS[] = {0, 2, 4, 8, 16, 32, 64, 128, 256};
