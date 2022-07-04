32a4adaf0229a080005fdb4b4c11f88e6fe79b3f
Chia-Hsiang Cheng <cche0109@student.monash.edu>
2022-07-04 11:52:38 +0800

    Fix bugs in CUDA arithmetic operations.

--- src/MathCUDA/CUDAHelpers.cu
+++ src/MathCUDA/CUDAHelpers.cu
@@ -4,51 +4,55 @@
 
 #define MIN(a, b) (a) < (b) ? (a) : (b)
 
-#define TWO_OPERAND_ARITHMETIC_FUNCTION_IMPLEMENTATION(func_name, output_type, op_type_1, op_type_2, helper_func)              \
-    void func_name(output_type *Dest, const op_type_1 *Operand1, const op_type_2 *Operand2, std::size_t size)                  \
-    {                                                                                                                          \
-        const std::size_t bytesOfOutput = size * sizeof(output_type);                                                          \
-        const std::size_t bytesOfArray1 = size * sizeof(op_type_1);                                                            \
-        const std::size_t bytesOfArray2 = size * sizeof(op_type_2);                                                            \
-        output_type *dest;                                                                                                     \
-        cudaMalloc(&dest, bytesOfOutput);                                                                                      \
-        op_type_1 *op1;                                                                                                        \
-        cudaMalloc(&op1, bytesOfArray1);                                                                                       \
-        op_type_2 *op2;                                                                                                        \
-        cudaMalloc(&op2, bytesOfArray2);                                                                                       \
-        const std::size_t numOfStreams = 8;                                                                                    \
-        const std::size_t arrayChunckSize = (size + numOfStreams - 1) / numOfStreams;                                          \
-        cudaStream_t streams[numOfStreams];                                                                                    \
-        for (std::size_t i = 0; i < numOfStreams; i++)                                                                         \
-            cudaStreamCreate(&streams[i]);                                                                                     \
-        for (std::size_t i = 0; i < numOfStreams; i++)                                                                         \
-        {                                                                                                                      \
-            const auto lowerBound = i * arrayChunckSize;                                                                       \
-            const auto upperBound = MIN(lowerBound + arrayChunckSize, size);                                                   \
-            const auto nElements = upperBound - lowerBound;                                                                    \
-            cudaMemcpyAsync(dest + lower, Dest + lower,                                                                        \
-                            sizeof(output_type) * nElements, cudaMemcpyHostToDevice,                                           \
-                            streams[i]);                                                                                       \
-            cudaMemcpyAsync(op1 + lower, Operand1 + lower,                                                                     \
-                            sizeof(op_type_1) * nElements, cudaMemcpyHostToDevice,                                             \
-                            streams[i]);                                                                                       \
-            cudaMemcpyAsync(op2 + lower, Operand2 + lower,                                                                     \
-                            sizeof(op_type_2) * nElements, cudaMemcpyHostToDevice,                                             \
-                            streams[i]);                                                                                       \
-            const std::size_t threadsPerBlock = nElements > 32 ? 32 : nElements;                                               \
-            const std::size_t blocksPerGrid = (nElements + threadsPerBlock - 1) / threadsPerBlock;                             \
-            helper_func<<<blocksPerGrid, threadsPerBlock, 0, streams[i]>>>(dest + lower, op1 + lower, op2 + lower, nElements); \
-            cudaMemcpyAsync(Dest + lower, dest + lower,                                                                        \
-                            sizeof(output_type) * nElements, cudaMemcpyDeviceToHost,                                           \
-                            streams[i]);                                                                                       \
-        }                                                                                                                      \
-        for (std::size_t i = 0; i < numOfStreams; i++)                                                                         \
-            cudaStreamSynchronize(streams[i]);                                                                                 \
-        for (std::size_t i = 0; i < numOfStreams; i++)                                                                         \
-            cudaStreamDestroy(streams[i]);                                                                                     \
-        cudaFree(dest);                                                                                                        \
-        cudaFree(op1);                                                                                                         \
-        cudaFree(op2);                                                                                                         \
+#define TWO_OPERAND_ARITHMETIC_FUNCTION_IMPLEMENTATION(func_name, output_type, op_type_1, op_type_2, helper_func) \
+    void func_name(output_type *Dest, const op_type_1 *Operand1, const op_type_2 *Operand2, std::size_t size)     \
+    {                                                                                                             \
+        const std::size_t bytesOfOutput = size * sizeof(output_type);                                             \
+        const std::size_t bytesOfArray1 = size * sizeof(op_type_1);                                               \
+        const std::size_t bytesOfArray2 = size * sizeof(op_type_2);                                               \
+        output_type *dest;                                                                                        \
+        cudaMalloc(&dest, bytesOfOutput);                                                                         \
+        op_type_1 *op1;                                                                                           \
+        cudaMalloc(&op1, bytesOfArray1);                                                                          \
+        op_type_2 *op2;                                                                                           \
+        cudaMalloc(&op2, bytesOfArray2);                                                                          \
+        const std::size_t numOfStreams = 8;                                                                       \
+        const std::size_t arrayChunckSize = (size + numOfStreams - 1) / numOfStreams;                             \
+        cudaStream_t streams[numOfStreams];                                                                       \
+        for (std::size_t i = 0; i < numOfStreams; i++)                                                            \
+            cudaStreamCreate(&streams[i]);                                                                        \
+        for (std::size_t i = 0; i < numOfStreams; i++)                                                            \
+        {                                                                                                         \
+            const auto lowerBound = i * arrayChunckSize;                                                          \
+            const auto upperBound = MIN(lowerBound + arrayChunckSize, size);                                      \
+            const auto nElements = upperBound - lowerBound;                                                       \
+            cudaMemcpyAsync(dest + lowerBound, Dest + lowerBound,                                                 \
+                            sizeof(output_type) * nElements, cudaMemcpyHostToDevice,                              \
+                            streams[i]);                                                                          \
+            cudaMemcpyAsync(op1 + lowerBound, Operand1 + lowerBound,                                              \
+                            sizeof(op_type_1) * nElements, cudaMemcpyHostToDevice,                                \
+                            streams[i]);                                                                          \
+            cudaMemcpyAsync(op2 + lowerBound, Operand2 + lowerBound,                                              \
+                            sizeof(op_type_2) * nElements, cudaMemcpyHostToDevice,                                \
+                            streams[i]);                                                                          \
+            const std::size_t threadsPerBlock = nElements > 32 ? 32 : (nElements > 0 ? nElements : 1);            \
+            const std::size_t blocksPerGrid = (nElements + threadsPerBlock - 1) / threadsPerBlock;                \
+            helper_func<<<blocksPerGrid, threadsPerBlock, 0, streams[i]>>>(                                       \
+                dest + lowerBound,                                                                                \
+                op1 + lowerBound,                                                                                 \
+                op2 + lowerBound,                                                                                 \
+                nElements);                                                                                       \
+            cudaMemcpyAsync(Dest + lowerBound, dest + lowerBound,                                                 \
+                            sizeof(output_type) * nElements, cudaMemcpyDeviceToHost,                              \
+                            streams[i]);                                                                          \
+        }                                                                                                         \
+        for (std::size_t i = 0; i < numOfStreams; i++)                                                            \
+            cudaStreamSynchronize(streams[i]);                                                                    \
+        for (std::size_t i = 0; i < numOfStreams; i++)                                                            \
+            cudaStreamDestroy(streams[i]);                                                                        \
+        cudaFree(dest);                                                                                           \
+        cudaFree(op1);                                                                                            \
+        cudaFree(op2);                                                                                            \
     }
 
 #define TWO_OPERAND_ARITHMETIC_FUNCTION_IMPLEMENTATIONS(func_name, helper_func)                    \
