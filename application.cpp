6c9d74fc342f3fefec173f2df4c2c8e2a40ae638
cche0109 <cche0109@student.monash.edu>
2022-07-08 09:52:45 +0800

    Train NeuralNetwork with linearly separable data.

--- application.cpp
+++ application.cpp
@@ -12,11 +12,19 @@ int main(void)
     Matrix<double> input({{2, 3, 9, 12}, {5, 2, 6, 5}});
     Matrix<double> labels({{0, 1, 0, 1}, {1, 0, 1, 0}});
     List<NeuralNetwork::LayerType> layerTypes({NeuralNetwork::LayerType::Linear, NeuralNetwork::LayerType::SoftMax});
-    List<Tuple<unsigned int>> shapes({{2, 2}, {2, 2}});
+    List<Tuple<unsigned int>> shapes({{2, 2}, {}});
     NeuralNetwork network(layerTypes, shapes, NeuralNetwork::LossType::NLL);
-    std::cout << network << std::endl;
+    std::cout << "Labels:" << std::endl;
+    std::cout << labels << std::endl;
+    std::cout << "Prediction Before Training:" << std::endl;
     std::cout << network.Predict(input) << std::endl;
-    const auto history = network.Train(input, labels, 1, 0.01);
-    std::cout << history << std::endl;
+    const auto epochs = 1000;
+    const double learningRate = 0.01;
+    const auto history = network.Train(input, labels, epochs, learningRate);
+    std::cout << "Loss History (First and Last):\n";
+    std::cout << history[0] << std::endl;
+    std::cout << history[history.Size() - 1] << std::endl;
+    std::cout << "Prediction After Training:" << std::endl;
+    std::cout << network.Predict(input) << std::endl;
     return 0;
 }
