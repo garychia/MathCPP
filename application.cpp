7bded23f14a47f3d014e53123a78ffff6f292360
cche0109 <cche0109@student.monash.edu>
2022-07-19 09:05:23 +0800

    Demonstrate how to use BatchNormalization layer

--- application.cpp
+++ application.cpp
@@ -11,8 +11,13 @@ int main(void)
 {
     Matrix<double> input({{2, 3, 9, 12}, {5, 2, 6, 5}});
     Matrix<double> labels({{0, 1, 0, 1}, {1, 0, 1, 0}});
-    List<NeuralNetwork::LayerType> layerTypes({NeuralNetwork::LayerType::Linear, NeuralNetwork::LayerType::SoftMax});
-    List<Tuple<unsigned int>> shapes({{2, 2}, {}});
+    List<NeuralNetwork::LayerType> layerTypes(
+        {
+            NeuralNetwork::LayerType::Linear,
+            NeuralNetwork::LayerType::BatchNormalization,
+            NeuralNetwork::LayerType::SoftMax
+        });
+    List<Tuple<unsigned int>> shapes({{2, 2}, {2}, {}});
     NeuralNetwork network(layerTypes, shapes, NeuralNetwork::LossType::NLL);
     std::cout << "Labels:" << std::endl;
     std::cout << labels << std::endl;
